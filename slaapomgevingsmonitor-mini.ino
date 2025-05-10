cc67766c0994b0c418c5408788067ea76a93f83e
martenschukkink <126795741+martenschukkink@users.noreply.github.com>
2025-05-10 12:51:11 +0200

    Temperature and light

--- slaapomgevingsmonitor-mini.ino
+++ slaapomgevingsmonitor-mini.ino
@@ -1,8 +1,10 @@
 #define TEMP_PIN A0
+#define LIGHT_PIN A1
 
 void  setup()
 {
   pinMode(TEMP_PIN, INPUT);
+  pinMode(LIGHT_PIN, INPUT);
 
   Serial.begin(9600);
   Serial.println("Initialising...");
@@ -11,12 +13,19 @@ void  setup()
 void loop()
 {
   // Temperatuur
-  int sensorInput = analogRead(TEMP_PIN);       // Uitgelezen waarde tussen 0 en 1023
-  double voltage = sensorInput / 1024.0 * 5.0;  // Omzetten naar voltage
-  double temperature = (voltage - 0.5) * 100;   // Omzetten naar temperatuur
+  int sensorInputTemp = analogRead(TEMP_PIN);       // Uitgelezen waarde tussen 0 en 1023
+  double voltage = sensorInputTemp / 1024.0 * 5.0;  // Omzetten naar voltage
+  double temperature = (voltage - 0.5) * 100;       // Omzetten naar temperatuur
 
-  Serial.print("Temperature:");
+  Serial.print("Temperatuur:");
   Serial.println(temperature, 2);
+
+  // Licht
+  int sensorInputLight = analogRead(LIGHT_PIN);     // Uitgelezen waarde tussen 0 en 1023
+  double light = sensorInputLight / 1024.0 * 100.0; // Omzetten naar percentage
+
+  Serial.print("Licht:");
+  Serial.println(light, 2);
 
   delay(1000);
 }
